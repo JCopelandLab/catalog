814ca97e22af14208e9b44a5d252c17f3113fed9
JCLab <JTCopeland@proton.me>
2024-03-24 18:29:05 -0400

    build out the code for a popup box. Adjust the current buttons to have a use-case that makes sense. Style the popup box and create functions to hide/show it based on button press. On button press(submit) take the entered information and create a new card on the page.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-"use strict";
+"use-strict";
+
+//variables for DOM manipulation
+
+//btns
+const createCard_btn = document.getElementById("new-btn");
+const submit_btn = document.getElementById("post-btn");
+
+//popup container
+const popupContainer = document.getElementById("popup-container");
+
+//input values
+const newCardName = document.getElementById("");
+const newCardDescription = document.getElementById("");
+
+//Functions for button functionality
+function closePopup() {
+  popupContainer.classList.remove("show-popup");
+  popupContainer.classList.add("hide-popup");
+  console.log("remove display");
+}
+
+function openPopup() {
+  popupContainer.classList.add("show-popup");
+  popupContainer.classList.remove("hide-popup");
+  console.log("add display");
+}
