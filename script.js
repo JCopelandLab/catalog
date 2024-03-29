e37ca498a86e0e1e9216405e1b1b95fc53254e11
JCLab <JTCopeland@proton.me>
2024-03-29 03:10:33 -0400

    Clean up the CSS due to edits made to the HTML file; some IDs and classes no longer exists, so refinement of repetitive styles should be consolidated. Construct functions ins JS to enable the desired outcome with button clicks. The last tasks includes sanitizing user inputs, validating them and dynamically structuring a new card then adding it into the projects container. A template structure must be formated.

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
 "use-strict";
+// buttons relative to card creation and  its deletion //
+const create_btn = document.getElementById("new-btn");
+const cardButtons = document.getElementById("buttons"); // card buttons container
+const delete_Btn = document.querySelectorAll("#deleteToggle");
+const read_btn = document.querySelectorAll("#readToggle");
 
-//variables for DOM manipulation
+// toggle read button colors
+read_btn.forEach(function (item, index, array) {
+  item.addEventListener("click", function () {
+    item.getAttribute("fill") == "red"
+      ? item.setAttribute("fill", "green")
+      : item.setAttribute("fill", "red");
+  });
+});
 
-//btns
-const createCard_btn = document.getElementById("new-btn");
-const submit_btn = document.getElementById("post-btn");
-
-//popup container
+// delete button to remove card
+delete_Btn.forEach(function (item, index, array) {
+  item.addEventListener("click", function () {
+    item.parentElement.parentElement.remove();
+  });
+});
+///////////////////////////////////////////////////////////////////////////////////////////
+//show popup function for new book button//
 const popupContainer = document.getElementById("popup-container");
-
-//input values
-const newCardName = document.getElementById("");
-const newCardDescription = document.getElementById("");
-
-//Functions for button functionality
-function closePopup() {
-  popupContainer.classList.remove("show-popup");
-  popupContainer.classList.add("hide-popup");
-  console.log("remove display");
-}
-
-function openPopup() {
+function toggleCardOn() {
   popupContainer.classList.add("show-popup");
   popupContainer.classList.remove("hide-popup");
-  console.log("add display");
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//form to bookCard construction
+const form = document.getElementById("form");
+let author, title, pages;
+const userAuthor = document.getElementById("card-author");
+const userTitle = document.getElementById("card-name");
+const userPageCount = document.getElementById("card-pages");
+const Library = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  author = userAuthor.value;
+  console.log(author);
+});
+
+function book(author, title, pages) {
+  this.author = author;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+// practicing object constructors
+/*
+function bookCatalog(author, title, pages) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+
+  this.test = function () {
+    return this.author;
+  };
+}
+
+const book1 = new bookCatalog("James Lee. Powh", "HixenVille", 222);
+console.log(book1.pages);
+console.log(book1.test());
+Object.getPrototypeOf(book1); //log it === bookCatalog.protoype ??
+book1.valueOf();
+// USE: Object.setPrototypeOf(book1.prototype, bookCatalog.prototype)
+// NOT: Enemy.prototype = Person.prototype;
+*/
