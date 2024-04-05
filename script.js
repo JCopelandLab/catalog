9b6f5b2a4e430a5e8459772f6010a688322bc370
JCLab <JTCopeland@proton.me>
2024-04-05 03:44:21 -0400

    Restructure the HTML, CSS and js documents to incorperate dynamic use of object constructors. Remove the static information used to display text to the user. Add additional input <textarea> to gather data from the user.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 "use-strict";
-const library = [];
 ////////////////////////////////////////////////////////////////////////////////////////////
 // toggle read button colors
 const readBtns = Array.from(document.querySelectorAll("#readBtn"));
@@ -21,12 +20,13 @@ deleteBtns.forEach(function (item, index) {
 ///////////////////////////////////////////////////////////////////////////////////////////
 //the hidden input form classList
 const newBookBtn = document.getElementById("new-btn");
-const popupContainer = document.getElementById("popup-container").classList;
+const popupContainer = document.getElementById("popup-container");
 
 //reveal input
 newBookBtn.addEventListener("click", function () {
-  if (popupContainer.contains("hide-popup")) {
-    popupContainer.remove("hide-popup") & popupContainer.add("show-popup");
+  if (popupContainer.classList.contains("hide-popup")) {
+    popupContainer.classList.remove("hide-popup") &
+      popupContainer.classList.add("show-popup");
   }
 });
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -35,6 +35,7 @@ let authorGlobal, titleGlobal, pagesGlobal;
 const form = document.getElementById("form");
 const userAuthorInput = document.getElementById("card-author");
 const userTitleInput = document.getElementById("card-name");
+const userDescription = document.getElementById("description");
 const userPagesInput = document.getElementById("card-pages");
 //////////////////////////////////////////////////////////////////////////////////////////
 //find checked radio button
@@ -48,19 +49,73 @@ function readProcessor() {
 }
 ///////////////////////////////////////////////////////////////////////////////////////////
 //projectS container and project framework
-const projects = document.getElementById("projects")
-const project = document.getElementById("project");
+const library = [
+  {
+    title: "Websters Dictonary",
+    author: "Noah Webster",
+    description:
+      "lorem tu yesi au husi fubi footbui nosi neighbor lamo nuis possimus ea mami animi assunduno",
+    pages: "490",
+    read: "Yes",
+  },
+];
+let str = "";
+const projects = document.getElementById("projects");
 //cdynamically create each objects(in array) html content
 function createProjectObject() {
-  library.forEach((obj) => {
-    let title = document.
+  library.forEach((obj, index) => {
+    str += `<div id="project" class="project">
+    <h3 id="title">${library[index].title}</h3>
+    <span id="authorName">${library[index].author}</span>
+    <p id="description">
+      ${library[index].description}
+    </p>
+    <p id="pageCount">${library[index].pages} Pages</p>
+
+    <div id="buttons">
+      <svg
+        id="deleteBtn"
+        fill="black"
+        type="button"
+        id="deleteToggle"
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M5.75 3V1.5h4.5V3zm-1.5 0V1a1 1 0 0 1 1-1h5.5a1 1 0 0 1 1 1v2h2.5a.75.75 0 0 1 0 1.5h-.365l-.743 9.653A2 2 0 0 1 11.148 16H4.852a2 2 0 0 1-1.994-1.847L2.115 4.5H1.75a.75.75 0 0 1 0-1.5zm-.63 1.5h8.76l-.734 9.538a.5.5 0 0 1-.498.462H4.852a.5.5 0 0 1-.498-.462z"
+          clip-rule="evenodd"
+        />
+      </svg>
+
+      <svg
+        id="readBtn"
+        fill="green"
+        type="button"
+        id="readToggle"
+        xmlns="http://www.w3.org/2000/svg"
+        width="1.5em"
+        height="1.5em"
+        viewBox="0 0 24 24"
+      >
+        <path
+          d="M6.012 18H21V4c0-1.103-.897-2-2-2H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.988 5 19.806 5 19s.55-.988 1.012-1M8 9h3V6h2v3h3v2h-3v3h-2v-3H8z"
+        />
+      </svg>
+    </div>
+  </div>`;
   });
+  document.getElementById("projects").innerHTML = str;
+  str = "";
 }
 ///////////////////////////////////////////////////////////////////////////////////////////
 //object for object constructor calls
-function createCard(author, title, pages, read) {
+function createCard(author, title, description, pages, read) {
   this.author = author.value;
   this.title = title.value;
+  this.description = description.value;
   this.pages = pages.value;
   this.read = read;
 }
@@ -72,23 +127,24 @@ form.addEventListener("submit", function (event) {
   const formData = new createCard(
     userAuthorInput,
     userTitleInput,
+    userDescription,
     userPagesInput,
     readProcessor()
   );
 
   library.unshift(formData);
+  console.log(library);
 
   createProjectObject();
 
-  if (popupContainer.contains("show-popup")) {
-    popupContainer.remove("show-popup") & popupContainer.add("hide-popup");
+  if (popupContainer.classList.contains("show-popup")) {
+    popupContainer.classList.remove("show-popup") &
+      popupContainer.classList.add("hide-popup");
     form.reset();
   }
 });
 
-// The project Library:: REcommended Steps
-//1> Create a function that loops the library array and dynamically creates the UI content for the userInput
-
-//2> Make the delete button remove the indexed item from the array and update the display
-// associate your DOM elements with the actual book objects in some way.
-// One easy solution is giving them a data-attribute that corresponds to the index of the library array.
+/*
+Add a button on each book’s display to remove the book from the library.
+You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
+*/
